e55ddb382fae5ad29f7ff0924a16ef54164b75d5
amanglani2503 <ambermanglani8871@gmail.com>
2025-01-25 12:25:39 +0530

    Implemented FullTimeEmployee class that extends Employee class

---
+++ Day06/src/problemstatements/employeemanagementsystem/FullTimeEmployee.java
@@ -0,0 +1,15 @@
+package problemstatements.employeemanagementsystem;
+
+class FullTimeEmployee extends Employee {
+    private double bonus;
+
+    public FullTimeEmployee(int employeeId, String name, double baseSalary, double bonus) {
+        super(employeeId, name, baseSalary);
+        this.bonus = bonus;
+    }
+
+    @Override
+    public double calculateSalary() {
+        return getBaseSalary() + bonus;
+    }
+}
